35484d6141ee553819842ce7c25f4492849f1aa0
U-lenovo-PC\lenovo <lenovo@lenovo-PC.(none)>
2014-04-17 21:27:12 +0800

    g5_url to g::url()

--- head.php
+++ head.php
@@ -9,7 +9,7 @@ if (!defined('_GNUBOARD_')) exit; // 개별 페이지 접근 불가
 		<div class='inner-header'>
 			<div class='header-960px'>
 				<h1 class='logo'>
-					<a href="<?php echo G5_URL ?>">
+					<a href="<?=g::url()?>">
 						<?if( file_exists( path_logo() ) ) { ?>
 							<img src="<?=url_logo()?>">
 						<?} else {?>
